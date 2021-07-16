d305105974c351bb96fc37f58471b96b75fbd4d1
RyanTwT354 <84882715+RyanTwT354@users.noreply.github.com>
2021-07-15 17:28:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+song = "";
+
+function preload() {
+     song = loadSound("music.mp3");
+}
+
+function setup() {
+canvas=createCanvas(600, 500);
+canvas.center();
+
+video = cerateCapture(VIDEO);
+video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function Play() {
+    song.Play();
+}
